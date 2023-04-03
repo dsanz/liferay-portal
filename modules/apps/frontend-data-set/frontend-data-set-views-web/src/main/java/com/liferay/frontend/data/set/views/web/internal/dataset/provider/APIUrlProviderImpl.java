7349731fe656f6600d642b08a51c013230e17268
Daniel Sanz <daniel.sanz@liferay.com>
2023-04-03 08:17:53 +0200

    LPS-184467 SF

--- modules/apps/frontend-data-set/frontend-data-set-views-web/src/main/java/com/liferay/frontend/data/set/views/web/internal/dataset/provider/APIUrlProviderImpl.java
+++ modules/apps/frontend-data-set/frontend-data-set-views-web/src/main/java/com/liferay/frontend/data/set/views/web/internal/dataset/provider/APIUrlProviderImpl.java
@@ -1,25 +1,40 @@
 /**
  * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
- * <p>
+ *
  * This library is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Lesser General Public License as published by the Free
  * Software Foundation; either version 2.1 of the License, or (at your option)
  * any later version.
- * <p>
+ *
  * This library is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
  */
 
-
 package com.liferay.frontend.data.set.views.web.internal.dataset.provider;
 
 import com.liferay.object.rest.dto.v1_0.ObjectEntry;
 
+import org.osgi.service.component.annotations.Component;
+
 /**
  * @author Daniel Sanz
  */
-public interface APIUrlDatasetProvider {
-	String getApiUrl(ObjectEntry fdsView);
+@Component(service = APIUrlProvider.class)
+public class APIUrlProviderImpl implements APIUrlProvider {
+	/* main things to do here:
+	    - interpolate URL parameters with context values (siteId, userId)
+	    - add required nested fields depending on field mappings
+	    - add sorting options
+
+	  things we don't need to worry about (FDS manages them)
+	    - Add page numbers and items per page
+	    - Add odata query in case filters are pre-applied
+	 */
+	public String getApiUrl(ObjectEntry fdsView) {
+		return "/o/headless-commerce-admin-catalog/v1.0/products" +
+			"?nestedFields=skus,catalog";
+	}
+
 }
