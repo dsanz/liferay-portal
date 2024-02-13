8a9d6a07207198ab2cd7a95b552c8e13e7e28645
Daniel Sanz <daniel.sanz@liferay.com>
2024-02-13 19:14:56 +0100

    LPD-17128 various improvements

--- workspaces/liferay-sample-workspace/client-extensions/liferay-sample-theme-css-1/webpack.config.js
+++ workspaces/liferay-sample-workspace/client-extensions/liferay-sample-theme-css-1/webpack.config.js
@@ -1,13 +1,23 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+const FileManagerPlugin = require('filemanager-webpack-plugin');
 const fs = require('fs');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
 const RemoveEmptyScriptsPlugin = require('webpack-remove-empty-scripts');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const buildThemeDir = path.join(__dirname, '/build/buildTheme/');
+const sourceCSSDir = path.join(__dirname, "src", "css");
+
 
 module.exports = {
 	context: path.join(__dirname, '/build/buildTheme/css/'),
 	entry: {
-		main: './main.css',
 		clay: './clay.css',
+		main: './main.css'
 	},
 	mode: 'production',
 	module: {
@@ -27,8 +37,7 @@ module.exports = {
 										return false;
 									}
 
-									console.log("  url " + url + " → " + path.join(__dirname, "src", "css", url))
-									if (!fs.existsSync(path.join(__dirname, "src", "css", url))) {
+									if (!fs.existsSync(path.join(sourceCSSDir, url))) {
 										return false;
 									}
 
@@ -40,23 +49,32 @@ module.exports = {
 				]
 			},
 			{
+				generator: {
+					filename: 'assets/[name].[contenthash][ext]'
+				},
 				test: /\.(png|jpe?g|gif|svg|eot|ttf|woff|woff2)$/i,
 				type: "asset/resource",
 			},
 		],
 	},
 	output: {
+		//assetModuleFilename: '../assets/[name].[contenthash][ext]',
 		clean: {
 			keep(asset) {
 				return !['main.css', 'main_rtl.css', 'clay.css', 'clay_rtl.css'].some((file) => asset.includes(file));
-			},
-			//dry: true, // Log the assets that should be removed instead of deleting them.
+			}
 		},
-		assetModuleFilename: 'assets/[name].[contenthash][ext]',
-		path: path.resolve(__dirname, 'build/buildTheme/css/')
-		//path: path.resolve(__dirname, 'build/liferay-client-extension-build/static2')
+		path: buildThemeDir
+		//path: path.resolve(__dirname, 'build/buildTheme/css/')
 	},
 	plugins: [
+		new FileManagerPlugin({
+			events: {
+				onStart: {
+					delete: [path.resolve(__dirname, 'build/buildTheme/assets'), path.resolve(__dirname, 'build/buildTheme/css/') + '/main.?*.css', path.resolve(__dirname, 'build/buildTheme/css/') + '/clay.?*.css',  ],
+				}
+			},
+		}),
 		new RemoveEmptyScriptsPlugin(),
 		new MiniCssExtractPlugin({
 			filename: "[name].[contenthash].css"
