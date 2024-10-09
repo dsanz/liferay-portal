f19a94ea864782708fa8de1eb8a9da0ed13dae50
Daniel Sanz <daniel.sanz@liferay.com>
2024-10-09 12:39:15 +0200

    LPD-37788 Add FDS action APIs to expose back-end action providers to the DSM. Note these are for frontend actions such as item actions and creation menu, as opposed to their counterparts in provider.action package

--- modules/apps/frontend-data-set/frontend-data-set-api/src/main/java/com/liferay/frontend/data/set/action/FDSItemActionListRegistry.java
+++ modules/apps/frontend-data-set/frontend-data-set-api/src/main/java/com/liferay/frontend/data/set/action/FDSItemActionListRegistry.java
@@ -3,13 +3,13 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.frontend.data.set;
+package com.liferay.frontend.data.set.action;
 
 /**
  * @author Daniel Sanz
  */
-public interface DataSetRegistry {
+public interface FDSItemActionListRegistry {
 
-	public DataSet getDataSet(String fdsName);
+	public FDSItemActionList getFDSItemActionList(String fdsName);
 
 }
