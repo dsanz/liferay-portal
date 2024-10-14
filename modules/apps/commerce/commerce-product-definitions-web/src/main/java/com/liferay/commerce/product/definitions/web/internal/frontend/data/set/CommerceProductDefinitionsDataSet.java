fc8c6ce06139ed5b753edb3155530810b2c586b5
Daniel Sanz <daniel.sanz@liferay.com>
2024-10-14 13:48:43 +0200

    LPD-37788 Implement Data Set API, client side. This exposes product definition data set to the Data Set Manager application

---
+++ modules/apps/commerce/commerce-product-definitions-web/src/main/java/com/liferay/commerce/product/definitions/web/internal/frontend/data/set/CommerceProductDefinitionsDataSet.java
@@ -0,0 +1,47 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.commerce.product.definitions.web.internal.frontend.data.set;
+
+import com.liferay.commerce.product.definitions.web.internal.constants.CommerceProductFDSNames;
+import com.liferay.frontend.data.set.DataSet;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Daniel Sanz
+ */
+@Component(
+	property = "frontend.data.set.name=" + CommerceProductFDSNames.PRODUCT_DEFINITIONS,
+	service = DataSet.class
+)
+public class CommerceProductDefinitionsDataSet implements DataSet {
+
+	@Override
+	public String getAdditionalAPIURLParameters() {
+		return "nestedFields=skus,catalog";
+	}
+
+	@Override
+	public String getName() {
+		return "Product Definitions";
+	}
+
+	@Override
+	public String getRESTApplication() {
+		return "/headless-commerce-admin-catalog/v1.0";
+	}
+
+	@Override
+	public String getRESTEndpoint() {
+		return "/v1.0/products";
+	}
+
+	@Override
+	public String getRESTSchema() {
+		return "Product";
+	}
+
+}
