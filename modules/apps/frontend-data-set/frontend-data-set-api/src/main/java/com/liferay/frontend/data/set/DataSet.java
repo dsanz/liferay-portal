2f11c39e261a07f9e3f8cc0d7aeffa6dcbb23257
Daniel Sanz <daniel.sanz@liferay.com>
2024-10-14 16:43:31 +0200

    LPD-37788 Create Data Set API to expose Java-based data sets to the Data Set Manager

---
+++ modules/apps/frontend-data-set/frontend-data-set-api/src/main/java/com/liferay/frontend/data/set/DataSet.java
@@ -0,0 +1,31 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.frontend.data.set;
+
+/**
+ * @author Daniel Sanz
+ */
+public interface DataSet {
+
+	public String getAdditionalAPIURLParameters();
+
+	public default String getDefaultItemsPerPage() {
+		return "20";
+	}
+
+	public default String getListOfItemsPerPage() {
+		return "4, 8, 20, 40, 60";
+	}
+
+	public String getName();
+
+	public String getRESTApplication();
+
+	public String getRESTEndpoint();
+
+	public String getRESTSchema();
+
+}
