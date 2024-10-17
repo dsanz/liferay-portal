ccc6126fbf7cf9d640c04cb32cd43205f69e3660
Daniel Sanz <daniel.sanz@liferay.com>
2024-10-17 09:08:52 +0200

    LPD-37788 Make Picklist DataSet customizable

---
+++ modules/apps/object/object-web/src/main/java/com/liferay/object/web/internal/list/type/frontend/data/set/action/ListTypeDefinitionsItemActionList.java
@@ -0,0 +1,70 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.object.web.internal.list.type.frontend.data.set.action;
+
+import com.liferay.frontend.data.set.action.FDSItemActionList;
+import com.liferay.frontend.data.set.model.FDSActionDropdownItem;
+import com.liferay.list.type.model.ListTypeDefinition;
+import com.liferay.object.web.internal.list.type.constants.ListTypeFDSNames;
+import com.liferay.object.web.internal.list.type.display.context.ViewListTypeDefinitionsDisplayContext;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermission;
+
+import java.util.Collections;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Daniel Sanz
+ */
+@Component(
+	property = "frontend.data.set.name=" + ListTypeFDSNames.LIST_TYPE_DEFINITIONS,
+	service = FDSItemActionList.class
+)
+public class ListTypeDefinitionsItemActionList implements FDSItemActionList {
+
+	@Override
+	public List<FDSActionDropdownItem> getDropdownItems(
+			HttpServletRequest httpServletRequest,
+			HttpServletResponse httpServletResponse)
+		throws PortalException {
+
+		ViewListTypeDefinitionsDisplayContext
+			viewListTypeDefinitionsDisplayContext =
+				new ViewListTypeDefinitionsDisplayContext(
+					httpServletRequest,
+					_listTypeDefinitionModelResourcePermission);
+
+		try {
+			return viewListTypeDefinitionsDisplayContext.
+				getFDSActionDropdownItems();
+		}
+		catch (Exception exception) {
+			if (_log.isWarnEnabled()) {
+				_log.warn("Unable to get dropdown items", exception);
+			}
+		}
+
+		return Collections.emptyList();
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		ListTypeDefinitionsItemActionList.class);
+
+	@Reference(
+		target = "(model.class.name=com.liferay.list.type.model.ListTypeDefinition)"
+	)
+	private ModelResourcePermission<ListTypeDefinition>
+		_listTypeDefinitionModelResourcePermission;
+
+}
