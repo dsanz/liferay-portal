6fc2d7247e18e220c0b1ef26c9a122c5de009b92
Daniel Sanz <daniel.sanz@liferay.com>
2025-04-30 20:02:32 +0200

    LPD-xxxx wip

--- modules/apps/frontend-data-set/frontend-data-set-web/src/main/resources/META-INF/resources/FrontendDataSet.js
+++ modules/apps/frontend-data-set/frontend-data-set-web/src/main/resources/META-INF/resources/FrontendDataSet.js
@@ -952,6 +952,36 @@ const FrontendDataSet = ({
 		}
 	};
 
+	const onDrop = (ev) => {
+		console.log("dropped: " + ev)
+		// Prevent default behavior (Prevent file from being opened)
+		ev.preventDefault();
+
+		if (ev.dataTransfer.items) {
+			// Use DataTransferItemList interface to access the file(s)
+			[...ev.dataTransfer.items].forEach((item, i) => {
+				// If dropped items aren't files, reject them
+				if (item.kind === "file") {
+					const file = item.getAsFile();
+					console.log(`… file[${i}].name = ${file.name}`);
+				}
+			});
+		} else {
+			// Use DataTransfer interface to access the file(s)
+			[...ev.dataTransfer.files].forEach((file, i) => {
+				console.log(`… file[${i}].name = ${file.name}`);
+			});
+		}
+	}
+
+
+const onDragOver = (event) => {
+	console.log("dragging over: " + event)
+
+	event.preventDefault();
+}
+
+
 	return (
 		<FrontendDataSetContext.Provider
 			value={{
@@ -1007,7 +1037,10 @@ const FrontendDataSet = ({
 			}}
 		>
 			<ViewsContext.Provider value={[viewsState, viewsDispatch]}>
-				<div className="fds">
+				<div className="fds"
+					 id="fds_drop_zone"
+					 onDrop={(event) => onDrop(event)}
+					 onDragOver={(event) => onDragOver(event)}>
 					<Modal id={dataSetSupportModalId} onClose={refreshData} />
 
 					{!sidePanelId && (
