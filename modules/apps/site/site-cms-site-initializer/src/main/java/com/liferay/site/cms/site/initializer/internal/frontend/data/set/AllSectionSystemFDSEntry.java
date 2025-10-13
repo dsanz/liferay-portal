ddc7f367888c53e34e214a0dcd7117b31083ed49
Daniel Sanz <daniel.sanz@liferay.com>
2025-10-13 18:38:02 +0200

    LPD-65557 Make AllSectionSystemFDSEntry able to return additional API URL parameters. This is a key property for all CMS data sets as many of them use the same endpoint, just with different parameters

--- modules/apps/site/site-cms-site-initializer/src/main/java/com/liferay/site/cms/site/initializer/internal/frontend/data/set/AllSectionSystemFDSEntry.java
+++ modules/apps/site/site-cms-site-initializer/src/main/java/com/liferay/site/cms/site/initializer/internal/frontend/data/set/AllSectionSystemFDSEntry.java
@@ -3,12 +3,21 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-
 package com.liferay.site.cms.site.initializer.internal.frontend.data.set;
 
 import com.liferay.frontend.data.set.SystemFDSEntry;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.site.cms.site.initializer.internal.constants.CMSSiteInitializerFDSNames;
+import com.liferay.site.cms.site.initializer.internal.display.context.ViewAllSectionDisplayContext;
+import com.liferay.site.cms.site.initializer.internal.fragment.renderer.BaseJSPSectionFragmentRenderer;
+
+import jakarta.servlet.http.HttpServletRequest;
+
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Daniel Sanz
@@ -18,6 +27,28 @@ import org.osgi.service.component.annotations.Component;
 	service = SystemFDSEntry.class
 )
 public class AllSectionSystemFDSEntry implements SystemFDSEntry {
+
+	@Override
+	public String getAdditionalAPIURLParameters(
+		HttpServletRequest httpServletRequest) {
+
+		try {
+			ViewAllSectionDisplayContext viewAllSectionDisplayContext =
+				_baseJSPSectionFragmentRenderer.getDisplayContext(
+					httpServletRequest);
+
+			String apiURL = viewAllSectionDisplayContext.getAPIURL();
+
+			return apiURL.substring(apiURL.lastIndexOf('?') + 1);
+		}
+		catch (PortalException portalException) {
+			_log.error(
+				"Unable to get additional API URL parameters", portalException);
+		}
+
+		return StringPool.BLANK;
+	}
+
 	@Override
 	public int getDefaultItemsPerPage() {
 		return 20;
@@ -35,12 +66,13 @@ public class AllSectionSystemFDSEntry implements SystemFDSEntry {
 
 	@Override
 	public String getPropsTransformer() {
-		return "{AssetsFilesDropFDSPropsTransformer} from site-cms-site-initializer";
+		return "{AssetsFilesDropFDSPropsTransformer} from " +
+			"site-cms-site-initializer";
 	}
 
 	@Override
 	public String getRESTApplication() {
-		return "/search/v1.0/";
+		return "/search/v1.0";
 	}
 
 	@Override
@@ -62,4 +94,14 @@ public class AllSectionSystemFDSEntry implements SystemFDSEntry {
 	public String getTitle() {
 		return "All Section";
 	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		AllSectionSystemFDSEntry.class);
+
+	@Reference(
+		target = "(frontend.data.set.name=" + CMSSiteInitializerFDSNames.ALL_SECTION + ")"
+	)
+	private BaseJSPSectionFragmentRenderer<ViewAllSectionDisplayContext>
+		_baseJSPSectionFragmentRenderer;
+
 }
