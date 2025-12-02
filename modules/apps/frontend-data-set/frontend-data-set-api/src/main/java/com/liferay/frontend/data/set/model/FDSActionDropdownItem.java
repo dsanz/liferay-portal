33d2a75d0dc00ea2b9c74a006a274b833f5627b5
Daniel Sanz <daniel.sanz@liferay.com>
2025-12-02 14:44:32 +0100

    LPD-70100 WIP Add FDSActionDropdownItemBuilder abstraction, from DropdownItemBuilder

--- modules/apps/frontend-data-set/frontend-data-set-api/src/main/java/com/liferay/frontend/data/set/model/FDSActionDropdownItem.java
+++ modules/apps/frontend-data-set/frontend-data-set-api/src/main/java/com/liferay/frontend/data/set/model/FDSActionDropdownItem.java
@@ -14,6 +14,9 @@ import java.util.Map;
  */
 public class FDSActionDropdownItem extends DropdownItem {
 
+	public FDSActionDropdownItem() {
+	}
+
 	public FDSActionDropdownItem(
 		String confirmationMessage, String confirmationMessageType,
 		String errorMessage, boolean highlighted, String href, String icon,
