e5ff37017208d3cb63183604b59a6c5421b965b3
Daniel Sanz <daniel.sanz@liferay.com>
2026-02-11 17:07:52 +0100

    LPD-78225 Fix. Style actions recursively as delete action now lives inside a group

--- modules/apps/site/site-cmp-site-initializer/src/main/resources/META-INF/resources/js/components/props_transformer/TasksFDSPropsTransformer.tsx
+++ modules/apps/site/site-cmp-site-initializer/src/main/resources/META-INF/resources/js/components/props_transformer/TasksFDSPropsTransformer.tsx
@@ -94,6 +94,19 @@ const WORKFLOW_TASK_MODALS: Record<
 	),
 };
 
+const styleActions = (actions: any[]): any[] =>
+	actions.map((action) => {
+		if (action?.data?.id === 'delete') {
+			action.className = 'text-danger';
+		}
+
+		if (action.items) {
+			action.items = styleActions(action.items);
+		}
+
+		return action;
+	});
+
 export default function TasksFDSPropsTransformer({
 	creationMenu,
 	id,
@@ -244,16 +257,7 @@ export default function TasksFDSPropsTransformer({
 			],
 		},
 		id,
-		itemsActions: itemsActions.map((action) => {
-			if (action?.data?.id === 'delete') {
-				return {
-					...action,
-					className: 'text-danger',
-				};
-			}
-
-			return action;
-		}),
+		itemsActions: styleActions(itemsActions),
 		async onActionDropdownItemClick({
 			action,
 			itemData,
