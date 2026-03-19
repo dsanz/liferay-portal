8d3a7fa0e97a1c3b12fff5ca3fd2235e201945fa
Daniel Sanz <daniel.sanz@liferay.com>
2026-03-19 13:52:45 +0100

    LPP-xxxxx Optimize parameter size

--- modules/apps/organizations/organizations-item-selector-api/src/main/java/com/liferay/organizations/item/selector/util/OrganizationsItemSelectorCriterionUtil.java
+++ modules/apps/organizations/organizations-item-selector-api/src/main/java/com/liferay/organizations/item/selector/util/OrganizationsItemSelectorCriterionUtil.java
@@ -8,19 +8,85 @@ package com.liferay.organizations.item.selector.util;
 
 import com.liferay.petra.function.transform.TransformUtil;
 import com.liferay.petra.string.StringUtil;
+
+import java.io.ByteArrayOutputStream;
+
+import java.nio.charset.StandardCharsets;
+
+import java.util.Base64;
 import java.util.List;
+import java.util.zip.Deflater;
+import java.util.zip.Inflater;
 
 /**
  * @author Daniel Sanz
  */
 public class OrganizationsItemSelectorCriterionUtil {
 	public static String toString(long[] selectedOrganizationIds) {
-		return StringUtil.merge(selectedOrganizationIds, "-");
+		String merged = StringUtil.merge(selectedOrganizationIds, "-");
+
+		return _compress(merged);
 	}
 
 	public static long[] toLongArray(String selectedOrganizationIds) {
-		List<String> ids = StringUtil.split(selectedOrganizationIds, '-');
+		String decompressed = _decompress(selectedOrganizationIds);
+
+		List<String> ids = StringUtil.split(decompressed, '-');
 
 		return TransformUtil.transformToLongArray(ids, Long::parseLong);
+	}
+
+	private static String _compress(String value) {
+		byte[] input = value.getBytes(StandardCharsets.UTF_8);
+
+		Deflater deflater = new Deflater();
+
+		deflater.setInput(input);
+		deflater.finish();
+
+		ByteArrayOutputStream byteArrayOutputStream =
+			new ByteArrayOutputStream(input.length);
+
+		byte[] buffer = new byte[1024];
+
+		while (!deflater.finished()) {
+			int count = deflater.deflate(buffer);
+
+			byteArrayOutputStream.write(buffer, 0, count);
+		}
+
+		deflater.end();
+
+		return Base64.getUrlEncoder().withoutPadding().encodeToString(
+			byteArrayOutputStream.toByteArray());
+	}
+
+	private static String _decompress(String value) {
+		try {
+			byte[] compressed = Base64.getUrlDecoder().decode(value);
+
+			Inflater inflater = new Inflater();
+
+			inflater.setInput(compressed);
+
+			ByteArrayOutputStream byteArrayOutputStream =
+				new ByteArrayOutputStream(compressed.length);
+
+			byte[] buffer = new byte[1024];
+
+			while (!inflater.finished()) {
+				int count = inflater.inflate(buffer);
+
+				byteArrayOutputStream.write(buffer, 0, count);
+			}
+
+			inflater.end();
+
+			return byteArrayOutputStream.toString(
+				StandardCharsets.UTF_8.name());
+		}
+		catch (Exception exception) {
+			throw new RuntimeException(exception);
+		}
 	}
 }
